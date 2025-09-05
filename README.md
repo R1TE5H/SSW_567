28416e5bb9fd47f9ae5e650e304e0e16238b7f82
Ritesh Persaud <riteshpersaud2003@gmail.com>
2025-09-05 11:11:50 -0400

    A-1 Completed

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SSW 567
 
 This is my repository for SSW 567 -- Quality Assurance and Maintenance
+
+To run tests, enter:
+```bash
+/Users/riteshpersaud/Desktop/SSW/SSW_567/.venv/bin/python -m pytest [PATH TO FILE OR DIR] -v -s
+```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-print("Hello World")
+from scripts.main import HelloWorld
 
 
 def test_print_output(capsys):
-    print("Hello World from test.py")
+    HelloWorld()
     captured = capsys.readouterr()
+    print("Capsys captured:", captured.out)
     assert "Hello World" in captured.out
